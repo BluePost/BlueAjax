3c50ddb56740498947fa4e53a128de724aa175a3
Freddie <freddie.poser@westminster.org.uk>
2016-02-16 10:56:34 +0000

    Added gitignore

--- functions/utils.php
+++ functions/utils.php
@@ -3,7 +3,7 @@
  * Create an error array that can then be json-encoded
  * @param type $text - The text of the error
  * @param type $index - The index of the error ("error")
- * @param type $orriginal - An array to start wit, will set the $index of that array to $text and return it
+ * @param type $orriginal - An array to start from, will set the $index of that array to $text and return it
  * @return Array
  */
 function error ($text, $index = "error", $orriginal = Array()) {
