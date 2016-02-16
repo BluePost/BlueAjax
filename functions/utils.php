bb0485dc66775973883b74ccff71e2d63ceebc5f
Freddie <freddie.poser@westminster.org.uk>
2016-02-16 10:54:14 +0000

    Updated error function

--- functions/utils.php
+++ functions/utils.php
@@ -3,8 +3,10 @@
  * Create an error array that can then be json-encoded
  * @param type $text - The text of the error
  * @param type $index - The index of the error ("error")
+ * @param type $orriginal - An array to start wit, will set the $index of that array to $text and return it
  * @return Array
  */
-function error ($text, $index = "error") {
-    return Array($index => $text);
+function error ($text, $index = "error", $orriginal = Array()) {
+    $orriginal[$index] = $text;
+    return $orriginal;
 }
